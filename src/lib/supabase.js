7096ef70b694c01cf3c98af1efca556f932a0cad
seha kim <epochsum@sehas-MacBook-Pro.local>
2025-07-19 17:13:31 +0900

    second commit

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://gufyuycbdwmurwhtvajq.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1Znl1eWNiZHdtdXJ3aHR2YWpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5MDE3MzAsImV4cCI6MjA2ODQ3NzczMH0.JNmUdz5KfOLGNHlN6G99eF5Kq1wm_lId0zSv4ZuUzZ0'; // wrapped in ''
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
